06ea15ce71ff53f0fb40581e5e0ff4f45abf4bdc
SantoshKilugu <kilugusanthoshkumar143@gmail.com>
2024-09-09 16:11:30 +0530

    intial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 3333;
+
+export const mongoDBURL="mongodb+srv://root:2005@books-store-mern.9rpgo.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
